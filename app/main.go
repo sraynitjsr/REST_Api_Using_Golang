1ae102dfd205384b960a24e3a47a353d3ed43129
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-01-21 23:41:37 +0530

    Graceful Shutdown Added

--- app/main.go
+++ app/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -21,5 +28,35 @@ func main() {
 		Handler: r,
 	}
 
-	myServer.ListenAndServe()
+	idleConnectionClosed := make(chan struct{})
+	go func() {
+		interruptSignal := make(chan os.Signal, 1)
+
+		signal.Notify(interruptSignal, os.Interrupt)
+		signal.Notify(interruptSignal, syscall.SIGTERM)
+		<-interruptSignal
+
+		log.Println("Service Interruppt Received")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+		defer cancel()
+
+		if err := myServer.Shutdown(ctx); err != nil {
+			log.Println("Error While Shutting Down Server", err)
+		}
+
+		log.Println("Shutdown Done Succesfuly")
+
+		close(idleConnectionClosed)
+	}()
+
+	log.Println("Starting Service at Port 8080")
+	if err := myServer.ListenAndServe(); err != nil {
+		if !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalln("Server Failed to Start", err)
+		}
+	}
+
+	<-idleConnectionClosed
+	log.Println("Service Stopped Succesfully")
 }
