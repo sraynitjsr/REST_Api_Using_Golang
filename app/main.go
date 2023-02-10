bbb2adf5d351603aad7b5ad4154d1bee33aa8bd8
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-02-10 21:46:50 +0530

    Modified Server

--- app/main.go
+++ app/main.go
@@ -1,62 +1,21 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-
-	"github.com/gorilla/mux"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome to the Home Page")
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello World</h1>")
+}
+
+func check(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Health check</h1>")
 }
 
 func main() {
-
-	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-
-	myServer := &http.Server{
-		Addr:    ":8080",
-		Handler: r,
-	}
-
-	idleConnectionClosed := make(chan struct{})
-	go func() {
-		interruptSignal := make(chan os.Signal, 1)
-
-		signal.Notify(interruptSignal, os.Interrupt)
-		signal.Notify(interruptSignal, syscall.SIGTERM)
-		<-interruptSignal
-
-		log.Println("Service Interruppt Received")
-
-		ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
-		defer cancel()
-
-		if err := myServer.Shutdown(ctx); err != nil {
-			log.Println("Error While Shutting Down Server", err)
-		}
-
-		log.Println("Shutdown Done Succesfuly")
-
-		close(idleConnectionClosed)
-	}()
-
-	log.Println("Starting Service at Port 8080")
-	if err := myServer.ListenAndServe(); err != nil {
-		if !errors.Is(err, http.ErrServerClosed) {
-			log.Fatalln("Server Failed to Start", err)
-		}
-	}
-
-	<-idleConnectionClosed
-	log.Println("Service Stopped Succesfully")
+	http.HandleFunc("/", index)
+	http.HandleFunc("/health_check", check)
+	fmt.Println("Server starting...")
+	http.ListenAndServe(":3000", nil)
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/sraynitjsr
 
 go 1.19
-
-require github.com/gorilla/mux v1.8.0
